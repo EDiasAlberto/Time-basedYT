9fed4834df0e10d61a52daa475262cf2c0f285bf
Ethan Dias Alberto <64550351+EDiasAlberto@users.noreply.github.com>
2021-10-10 15:00:32 +0100

    Add files via upload

--- linkFuncs.py
+++ linkFuncs.py
@@ -1,7 +1,9 @@
 from googleapiclient.discovery import build
-import datetime
+import datetime, pytz
 from os import getenv
 from dotenv import load_dotenv
+
+timezone = pytz.timezone(' '.join(pytz.country_timezones['gb']))
 
 load_dotenv()
 API_KEY = getenv("API_KEY")
@@ -10,7 +12,7 @@ API_KEY = getenv("API_KEY")
 def getLink(time):
     videoID = None
     if time is None:
-        time = datetime.datetime.now().strftime("%H:%M")
+        time = datetime.datetime.now(timezone).strftime("%H:%M")
     youtube = build("youtube", "v3", developerKey=API_KEY)
     request = youtube.search().list(part="snippet", maxResults=1, videoCategoryId=10, q=time, type="video")
     response = request.execute()
